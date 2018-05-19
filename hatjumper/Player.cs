9ced2181c1a7380ce652bd1be9eb39566f80527f
Владимир <vovaznaet@gmail.com>
2018-05-19 15:21:52 +1000

    добавлены изменени связанные с локациями и мелкие исправления

---
+++ hatjumper/Player.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace hatjumper
+{
+    class Player
+    {
+        int money, highScore;
+
+        private Player()
+        {
+
+        }
+
+        private static Player instance = new Player();
+
+        public static Player getInstance()
+        {
+            return instance;
+        }
+
+        public void loadData()
+        {
+            // с сохранением и загрузкой ебанина
+        }
+
+        public void saveData()
+        {
+
+        }
+
+        public bool checkHighScore(int score)
+        {
+            if (score > highScore)
+            {
+                highScore = score;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ hatjumper/Shop.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace hatjumper
+{
+    class Shop
+    {
+        private Shop()
+        {
+
+        }
+
+        private static Shop instance = new Shop();
+
+        public static Shop getInstance()
+        {
+            return instance;
+        }
+
+        public void loadData()
+        {
+            // с сохранением и загрузкой ебанина
+        }
+
+        public void saveData()
+        {
+
+        }
+    }
+}
